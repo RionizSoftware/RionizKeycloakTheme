765d764f5979f6c93325cf25ab49899b191f921e
uchar <omid.es73@gmail.com>
2024-09-23 14:32:37 +0330

    Eject sample login page for testing

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -1,11 +1,11 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import type { ClassKey } from "rionizkeycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "rionizkeycloakify/login/DefaultPage";
 import UserProfileFormFields from "rionizkeycloakify/login/UserProfileFormFields";
 import Template from "rionizkeycloakify/login/Template";
-
+const Login = lazy(() => import("./pages/Login"));
 const doMakeUserConfirmPassword = true;
 export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
@@ -14,6 +14,14 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl":
+                        return (
+                            <Login
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={true}
+                            />
+                        );
                     default:
                         return (
                             <DefaultPage
